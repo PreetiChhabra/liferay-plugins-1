2689e1d505e587ae35a8dde7bb8613d41405b206
Richard SINELLE <rsinelle@yahoo.fr>
2014-07-06 11:28:12 +0200

    update

--- contact/contact-portlet/src/main/webapp/WEB-INF/sql/indexes.sql
+++
@@ -1,4 +0,0 @@
-create index IX_ADA67CF9 on contact_Foo (field2);
-create index IX_2B843B07 on contact_Foo (uuid_);
-create index IX_3E6E6EC1 on contact_Foo (uuid_, companyId);
-create unique index IX_C695B183 on contact_Foo (uuid_, groupId);
--- contact/contact-portlet/src/main/webapp/WEB-INF/sql/tables.sql
+++ contact/contact-portlet/src/main/webapp/WEB-INF/sql/tables.sql
@@ -1,28 +1,3 @@
-create table contact_Foo (
-	uuid_ VARCHAR(75) null,
-	fooId LONG not null primary key,
-	groupId LONG,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createDate DATE null,
-	modifiedDate DATE null,
-	field1 VARCHAR(75) null,
-	field2 BOOLEAN,
-	field3 INTEGER,
-	field4 DATE null,
-	field5 VARCHAR(75) null
-);
-
-create table rsc_Contact (
-	personId LONG not null primary key,
-	companyId LONG,
-	lastName VARCHAR(75) null,
-	firstName VARCHAR(75) null,
-	mail VARCHAR(75) null,
-	phone VARCHAR(75) null
-);
-
 create table rsc_contact (
 	personId LONG not null primary key,
 	companyId LONG,
